4a49dbcbb41c7ebeeb09741490b9939333abb01f
Luis mi <alexanderlara1996@gmail.com>
2021-09-27 08:17:02 -0500

    Guardando cambios

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,39 @@
 @section('title','Home')
 
 @section('content')
-<!-- Navbar goes here -->
 
-<h1 class="text-5xl text-blue-500 text-center pt-24">Bienvenido a mi aplicación para el pago de sus cuotas.</h1>
+<h2>CIP VIRTUAL</h2>
+<p>En el cip virtual podra visualizar sus datos principales, sus comprobantes, realizar pagos de sus cuotas y tramitar sus certificados(A,B,C).</p>
+<div class="row">
+    <div class="col-xs-6">
+        <p class="lead">Perfil:</p>
+        <p class="text-muted well well-sm no-shadow" style="margin-top: 10px;">
+            Tendra un panel donde podra visualizar sus datos principales.
+        </p>
+    </div>
+    <div class="col-xs-6">
+        <p class="lead">Comprobantes de Pago</p>
+        <p class="text-muted well well-sm no-shadow" style="margin-top: 10px;">
+            En esta sección podra consulta de sus boletas, facturas electrónicas.
+        </p>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-xs-6">
+        <p class="lead">Realizar Pago</p>
+        <p class="text-muted well well-sm no-shadow" style="margin-top: 10px;">
+            En esta sección podra realizar sus pagos de cuotas ordinarias y certificados de tipo A, B, C.
+        </p>
+    </div>
+    <div class="col-xs-6">
+        <p class="lead">Metodos de Pago:</p>
+        <img src="{{asset('images/visa.png')}}" alt="Visa">
+        <img src="{{asset('images/mastercard.png')}}" alt="Mastercard">
+        <img src="{{asset('images/american-express.png')}}" alt="American Express">
+        <p class="text-muted well well-sm no-shadow" style="margin-top: 10px;">
+            Esta tienda está autorizada por Visa|Mastercard|American Express para realizar transacciones electrónicas.
+        </p>
+    </div>
+</div>
 @endsection
